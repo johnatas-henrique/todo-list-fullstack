17851df991ea51418f880a28c4bbb7e84dbf8492
Johnatas Henrique <johnatas.henrique@gmail.com>
2021-10-28 21:19:01 -0300

    feat: insere endpoint get para teste básico do server

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.API_PORT || 3000;
+
+app.get('/tasks', (req, res) => {
+  res.status(200).json({ message: 'ok', ping: req.body.ping });
+});
 
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
